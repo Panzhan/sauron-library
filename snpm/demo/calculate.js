cfc9937852f5ef8b6df3f491bbdc33d2be1611ba
sauron.pan <sauron.pan@klook.com>
2022-06-16 17:58:39 +0800

    feat: update

---
+++ snpm/demo/calculate.js
@@ -0,0 +1,25 @@
+// const getDecimalLength = ({ x, y } = {}) => {
+//     let lx, ly, lmax
+//     try {
+//         lx = x.toString().split('.')[1].length
+//     } catch {
+//         lx = 0
+//     }
+//     try {
+//         ly = y.toString().split('.')[1].length
+//     } catch {
+//         ly = 0
+//     }
+//     lmax = Math.max(lx, ly)
+//     return {
+//         lx, ly, lmax
+//     }
+// }
+
+// const arr = [1,2,3]
+
+// export { arr }
+
+export function sayHi(user) {
+  console.log(`Hello, ${user}!`);
+}
---
+++ snpm/demo/index.js
@@ -0,0 +1,5 @@
+// import { arr } from './calculate.js'
+// console.log('arr', arr)
+
+import { sayHi } from './calculate';
+sayHi('John'); // Hello, John!
